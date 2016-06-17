4e0c684c9588c86fae7c42969ade7af8d767d483
Jeff DiPallo <jdipallo@gmail.com>
2016-06-17 09:26:03 -0600

    StoryTime Mid-Term project

--- StoryTime/library.js
+++ StoryTime/library.js
@@ -11,7 +11,7 @@ angular.module('Library', [])
 function Book() {
 	this.isbn = Math.floor((Math.random() * 10000) + 1);
 	this.name = '';
-	this.cover = {};
+	this.cover = new Cover("Title", "Author", "book_cover_leather.png");;
 	this.pages = [new Page()];
 }
 
@@ -45,9 +45,10 @@ Page.prototype.setVoFilename = function(voFilename) {
 	this.voFilename = voFilename;
 }
 
-function Cover(title, author) {
+function Cover(title, author, imageCover) {
 	this.title = title;
 	this.author = author;
+	this.imageCover = imageCover;
 }
 
 // Our Book Factory
@@ -62,8 +63,8 @@ function bookFactory() {
 		return new Page(text, image, voFilename);
 	}
 
-	bookFactory.createCover = function(title, author) {
-		return new Cover(title, author);
+	bookFactory.createCover = function(title, author, imageCover) {
+		return new Cover(title, author, imageCover);
 	}
 
 	bookFactory.createEmptyPage = function() {
@@ -74,28 +75,28 @@ function bookFactory() {
 	// return an array of books - taking in an userID or libraryID
 	bookFactory.retrieveBooks = function() {
 		// 1st dummy up data
-		var cover = new Cover("Jeff's Book", "Jeff DiPallo");
-		var page1 = new Page("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters",
-			 'userId/images');
-		var page2 = new Page("last page - There are many variations believable. If you are going to use dden in the middle of text. All the Lorem Ipsum generators on the Internet", 'userId/images', '');
-		var pages = [page1, page2];
+		var cover = new Cover("Green Eggs & Ham", "Dr. Seuss", "green-eggs-and-ham.jpg");
+		var page1 = new Page("I am Sam.\nI am Sam. Sam I am.\n That Sam-I-am.\n That Sam-I-am!\n I do not like that Sam-I-am.\nDo you like green eggs and ham?", '', "StorytimeVO-1060_1.wav");
+		var page2 = new Page("I do not like them, Sam-I-am.\nI do not like green eggs and ham.\nWould you like them Here or there?\nI would not like them anywhere.", '', "StorytimeVO-1060_2.wav");
+		var page3 = new Page("I would not like them anywhere.\nI do not like green eggs and ham.\nI do not like them, Sam-I-Am.\nWould you like them in a house?");
+		var page4 = new Page("Would you like them with a mouse?\nI do not like them in a house.\nI do not like them with a mouse.");
+		var pages = [page1, page2, page3, page4];
 		var book1 = new Book();
-		book1.setName('Book 1')
+		book1.setName('Green Eggs & Ham')
 		book1.setCover(cover);
 		book1.addPages(pages)
 
 		// 2nd dummy....
-	 	cover = new Cover("Jan's Book Book", "Jan Berenstain");
-		page1 = new Page("Book2----There are many variations of passages of Lorem Ipsum available,  by injecightly believable. se a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet", 'userId/images', '');
-	 	page2 = new Page("Book2----page 22222222 tions of passages of Lorem Ipsum available, our, or ightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet", 'userId/images', '');
-	 	page3 = new Page("Book2----last page-here are many variations of passages of Lorem Ipsum available, buumour, or randly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet", 'userId/images', '');
-	 	pages = [page1, page2, page3];
+	 	cover = new Cover("Goodnight Moon", "Margaret Wise Brown", "goodnight-moon.jpg");
+		page1 = new Page("In the great green room\nthere was a telephone\nAnd a red balloon\nAnd a picture of...\nThe cow jumping over the moon\nand there were three little bears, sitting on chairs");
+	 	page2 = new Page("and two little kittens and a pair of mittens\nand a little toy house and a young mouse\nand a comb and a brush and bowl full of mush\nand a quiet old lady who was whispering hush.");
+	 	pages = [page1, page2];
 		book2 = new Book();
-		book2.setName('Book 2')
+		book2.setName('Goodnight Moon')
 		book2.setCover(cover);
 		book2.addPages(pages)
 
-	 	cover = new Cover("Melvin's Book", "Melvin Berger");
+	 	cover = new Cover("Melvin's Book", "Melvin Berger", "book_cover_leather.png");
 		page1 = new Page("Book 3--There are many variations of passages of Lorem Ipsum ou are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet", 'userId/images', '');
 	 	page2 = new Page("Book 3 - page 2222222 orem Ipsum available, bted humour, or vable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet", 'userId/images', '');
 	 	page3 = new Page("Book 3Tpage 33333333  ble, d humvable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet", 'userId/images', '');
